b9e2c5c9af64537907c75b028420399939250af3
Dan Miller <x.dan.miller@outlook.com>
2014-02-23 13:19:27 -0500

    Implemented: Add returns value for string with single value.

--- StringCalculatorKata.Logic/StringCalculator.fs
+++ StringCalculatorKata.Logic/StringCalculator.fs
@@ -1,4 +1,8 @@
 ﻿namespace StringCalculatorKata.Logic
 
+open System
+
 type StringCalculator() = 
-    member x.Add stringValue = 0
+    member x.Add stringValue = 
+        if String.IsNullOrEmpty stringValue then 0
+        else Int32.Parse stringValue 
--- StringCalculatorKata.Tests/StringCalculatorTests.fs
+++ StringCalculatorKata.Tests/StringCalculatorTests.fs
@@ -6,8 +6,16 @@ open StringCalculatorKata.Logic
 
 [<TestFixture>]
 type StringCalculatorTests() = 
+
     [<Test>]
     member x.Add_Returns_0_For_Empty_String() =
         let calc = StringCalculator()
         let result = calc.Add ""
         Assert.That(result, Is.EqualTo 0)
+
+    [<Test>]
+    member x.Add_Returns_Value_For_String_With_Single_Value() =
+        let calc = StringCalculator()
+        let result = calc.Add "1"
+        let expectedValue = 1
+        Assert.That(result, Is.EqualTo expectedValue)
